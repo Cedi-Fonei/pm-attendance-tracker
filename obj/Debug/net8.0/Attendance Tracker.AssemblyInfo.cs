b1be9fc20559a0527b311aab196dddbdc54c0f96
Cedi-Fonei <cedinfinity@gmail.com>
2026-01-03 12:10:48 -0500

    gitignore fix for bin and obj folders

--- obj/Debug/net8.0/Attendance Tracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/Attendance Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Attendance Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ca6ddb75f647d6d057ba6f2912c485f53b7266")]
 [assembly: System.Reflection.AssemblyProductAttribute("Attendance Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Attendance Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
